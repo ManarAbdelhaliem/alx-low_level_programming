026a3503f94e8bde47843cc6e7f6782e3c0f8f1b
ManarAbdelhaliem <manarabdelhaliem138@gmail.com>
2023-09-13 03:18:04 -0700

    update this file

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,37 +1,29 @@
-#include "function_pointers.h"
-#include <stdlib.h>
-#include <stdio.h>
 #include "3-calc.h"
 /**
-* main - Prints the result of simple operations.
-* @argc: The number of arguments supplied to the program.
-* @argv: An array of pointers to the arguments.
-*
-* Return: Always 0.
+ * main - main function
+ * @argv: vector array
+ * @argc: length
+ * Return: 0
 */
-int main(int attribute((__unused_)) argc, char *argv[])
+int main(int argc, char *argv[])
 {
-int num1, num2;
-char *op;
-if (argc != 4)
-{
-printf("Error\n");
-exit(98);
-}
-num1 = atoi(argv[1]);
-op = argv[2];
-num2 = atoi(argv[3]);
-if (get_op_func(op) == NULL || op[1] != '\0')
-{
-printf("Error\n");
-exit(99);
-}
-if ((*op == '/' && num2 == 0) ||
-(*op == '%' && num2 == 0))
-{
-printf("Error\n");
-exit(100);
-}
-printf("%d\n", get_op_func(op)(num1, num2));
-return (0);
+	int first_oper, second_oper;
+
+	int (*operation)(int, int);
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	first_oper = atoi(argv[1]);
+	second_oper = atoi(argv[3]);
+	operation = get_op_func(argv[2]);
+	if (!operation)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+	printf("%d\n", operation(first_oper, second_oper));
+	return (0);
 }

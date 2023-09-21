3cd608823472a34ca4fcd35f713fa82b38e1329f
ManarAbdelhaliem <134805382+ManarAbdelhaliem@users.noreply.github.com>
2023-09-21 16:36:53 +0300

    Update 100-first.css

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,11 +1,13 @@
-#include "lists.h"
-/**
-  * reverse_listint - define function
-  * @head: describe argument
-  * Return: what does it return?
-  */
-listint_t *reverse_listint(listint_t **head)
-{
-	head++;
-	return (0);
+#include <stdio.h>
+
+void myConstructor(void) __attribute__((constructor));
+
+void myConstructor(void) {
+    printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}
+
+int main() {
+    // Your main program logic here
+    printf("Main function executed.\n");
+    return 0;
 }

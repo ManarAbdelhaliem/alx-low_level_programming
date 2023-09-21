887b9ec53f3e65f56be823dc6ae0d107653a9760
ManarAbdelhaliem <134805382+ManarAbdelhaliem@users.noreply.github.com>
2023-09-21 16:38:06 +0300

    Update 100-first.cvf

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 
-void myConstructor(void) __attribute__((constructor));
-
-void myConstructor(void) {
+void printMessage() {
     printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
 }
 
 int main() {
+    // Call the function to print the message before main
+    printMessage();
+
     // Your main program logic here
     printf("Main function executed.\n");
     return 0;

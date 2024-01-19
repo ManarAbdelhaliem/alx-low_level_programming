2c4e132bab3fca1eccd486c25d807ae04d839960
ManarAbdelhaliem <134805382+ManarAbdelhaliem@users.noreply.github.com>
2024-01-19 11:08:15 +0200

    Delete 0x18-dynamic_libraries/_strspn.c

--- 0x18-dynamic_libraries/_strspn.c
+++
@@ -1,7 +0,0 @@
-/* _strspn.c */
-#include "main.h"
-unsigned int _strspn(char *s, char *accept) {
-    unsigned int length = 0;
-    while (*s && _strchr(accept, *s++)) length++;
-    return length;
-}

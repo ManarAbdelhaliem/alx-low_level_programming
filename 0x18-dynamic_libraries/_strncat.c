5815d64ecd6b4661966a82e88b8898b348fba7c3
ManarAbdelhaliem <134805382+ManarAbdelhaliem@users.noreply.github.com>
2024-01-19 11:06:38 +0200

    Delete 0x18-dynamic_libraries/_strncat.c

--- 0x18-dynamic_libraries/_strncat.c
+++
@@ -1,9 +0,0 @@
-/* _strncat.c */
-#include "main.h"
-char *_strncat(char *dest, char *src, int n) {
-    char *start = dest;
-    while (*dest) dest++;
-    while (n-- && *src) *dest++ = *src++;
-    *dest = '\0';
-    return start;
-}
